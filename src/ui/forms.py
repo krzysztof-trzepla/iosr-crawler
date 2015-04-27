b771454ee731ff527999bf5c479cd4d69ebfae49
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-04-27 22:29:22 +0200

    Add bootstrap theme.

--- src/ui/forms.py
+++ src/ui/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 
 class SentenceForm(forms.Form):
-    sentence = forms.CharField(label='Sentence', max_length=100)
+    sentence = forms.CharField(label='Sentence', max_length=100,
+                               widget=forms.TextInput(
+                                   attrs={'class': 'form-control'}))
