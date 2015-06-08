9bf4bb11443fe8ac024563852f8aaf2804d080fd
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-06-08 10:59:51 +0200

    Add travis continuous integration.

--- src/riak_setup.sh
+++ src/riak_setup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-../riak-2.0.5/bin/riak start
-../riak-2.0.5/bin/riak-admin bucket-type create set '{"n_val":3, "props":{"datatype":"set"}}'
-../riak-2.0.5/bin/riak-admin bucket-type activate set
-../riak-2.0.5/bin/riak-admin bucket-type create counter '{"n_val":3, "props":{"datatype":"counter"}}'
-../riak-2.0.5/bin/riak-admin bucket-type activate counter
+riak-admin bucket-type create set '{"n_val":3, "props":{"datatype":"set"}}'
+riak-admin bucket-type activate set
